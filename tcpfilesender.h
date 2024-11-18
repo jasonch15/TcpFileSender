cce67358a1d7365381d8c4a5c9fb425f197d3a6b
DESKTOP-SAU9E0O\User <jasonchuang15@gmail.com>
2024-11-18 23:46:01 +0800

    修改為可自行新曾port and ip

--- tcpfilesender.h
+++ tcpfilesender.h
@@ -1,5 +1,5 @@
-#ifndef TCPFILESENDER_H
-#define TCPFILESENDER_H
+#ifndef TCP_FILE_SENDER_H
+#define TCP_FILE_SENDER_H
 
 #include <QDialog>
 #include <QtNetwork>
@@ -10,29 +10,41 @@ class TcpFileSender : public QDialog
     Q_OBJECT
 
 public:
-    TcpFileSender(QWidget *parent = 0);
+    explicit TcpFileSender(QWidget *parent = nullptr);
     ~TcpFileSender();
-public slots:
-    void start();
-    void startTransfer();
-    void updateClientProgress(qint64 numBytes);
-    void openFile();
-private:
-    QProgressBar     *clientProgressBar;
-    QLabel           *clientStatusLabel;
-    QPushButton      *startButton;
-    QPushButton      *quitButton;
-    QPushButton      *openButton;
-    QDialogButtonBox *buttonBox;
-    QTcpSocket       tcpClient;
 
-    qint64           totalBytes;
-    qint64           bytesWritten;
-    qint64           bytesToWrite;
-    qint64           loadSize;
-    QString          fileName;
-    QFile            *localFile;
-    QByteArray       outBlock;
+public slots:
+    void start();                      // 开始连接服务器并传输文件
+    void startTransfer();              // 开始传输文件
+    void updateClientProgress(qint64 numBytes); // 更新客户端进度条
+    void openFile();                   // 打开文件
+
+private:
+    // UI 控件
+    QProgressBar *clientProgressBar;
+    QLabel *clientStatusLabel;
+    QPushButton *startButton;
+    QPushButton *quitButton;
+    QPushButton *openButton;
+    QDialogButtonBox *buttonBox;
+
+    // 新增的 IP 和端口相关控件
+    QLabel *ipLabel;
+    QLineEdit *ipLineEdit;
+    QLabel *portLabel;
+    QLineEdit *portLineEdit;
+
+    // TCP 客户端相关
+    QTcpSocket tcpClient;
+
+    // 文件传输相关变量
+    qint64 totalBytes;
+    qint64 bytesWritten;
+    qint64 bytesToWrite;
+    qint64 loadSize;
+    QString fileName;
+    QFile *localFile;
+    QByteArray outBlock;
 };
 
-#endif // TCPFILESENDER_H
+#endif // TCP_FILE_SENDER_H
